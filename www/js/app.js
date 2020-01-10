21394a3e7775a28f48311d6e9e6a86070b418e0e
HinaYashvi <hina.parmar@staroneweb.co.in>
2020-01-10 17:00:42 +0530

    first commit

--- www/js/app.js
+++ www/js/app.js
@@ -615,7 +615,7 @@ function onSuccess(position){
       url:"https://maps.googleapis.com/maps/api/place/textsearch/json?query="+latitude+longitude+"&key=AIzaSyCfIHJQnEnmC-s6OO9qaymRe6dKG4l0T1s",
       //data:{'latitude':latitude,'longitude':longitude},
       success:function(resLoc){
-        alert(results.formatted_address);
+        alert(resLoc.status"@@@@");
         //var resjson = $.parseJSON(resLoc);
         var add_res = resLoc.html_attributions;
         $("#map-canvas").html(resLoc+" ^^^^^^^^^^^^<br/>");
