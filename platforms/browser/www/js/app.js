e78a21f4236ed1f037ab1209e4b37fe230c260f3
HinaYashvi <hina.parmar@staroneweb.co.in>
2020-01-13 09:31:40 +0530

    first commit

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -609,28 +609,34 @@ function onSuccess(position){
           'Timestamp: '         + position.timestamp                + '\n');
     var longitude = position.coords.longitude;
     var latitude = position.coords.latitude;
-
-     $.ajax({
-      type:'POST', 
-      url:"https://maps.googleapis.com/maps/api/place/textsearch/json?query="+latitude+longitude+"&key=AIzaSyCfIHJQnEnmC-s6OO9qaymRe6dKG4l0T1s",
+    //alert("https://maps.googleapis.com/maps/api/place/textsearch/json?query="+latitude+longitude+"&key=AIzaSyCfIHJQnEnmC-s6OO9qaymRe6dKG4l0T1s");
+     /*$.ajax({
+      type:'GET',
+      dataType: "json", 
+      url:"https://maps.googleapis.com/maps/api/place/textsearch/json?query="+latitude+longitude+"&key=AIzaSyAdbMPmV9BqtRFcHEHCoe1fgpU2mEA1UnM",
+      //AIzaSyAdbMPmV9BqtRFcHEHCoe1fgpU2mEA1UnM (SIR) //
       //data:{'latitude':latitude,'longitude':longitude},
       success:function(resLoc){
-        alert(resLoc.status+"@@@@");
-        alert(status+"status");
-        //var resjson = $.parseJSON(resLoc);
-        var add_res = resLoc.html_attributions;
-        $("#map-canvas").html(resLoc+" ^^^^^^^^^^^^<br/>");
-        //var add_array = add_res[0]; 
-
+        alert(resLoc);
+        //alert(status+"status");
+        var resjson = $.parseJSON(resLoc);
+        alert("success "+resLoc);
+        alert("resjson "+resjson);
+        /*var add_res = resLoc.results;
+        document.writeln(add_res);
+        var add_array = add_res.formatted_address; 
+        var add_array1 = add_res['formatted_address']; 
         //var formatted_address = add_res.formatted_address;
 
         
-        //alert("formatted_address "+formatted_address);
-        //$("#map-canvas").html(formatted_address+" ^^^^^^^^^^^^");
+        alert("add_array "+add_array);
+        alert("add_array1 "+add_array1);*/
+ /*       $("#map-canvas").html(resLoc+" ^^^^^^^^^^^^");
+        
       }
-    });
+    });*/
 
-    /*$.ajax({
+    $.ajax({
       type:'POST', 
       url:base_url+'APP/Appcontroller/getLocation',
       data:{'latitude':latitude,'longitude':longitude},
@@ -638,7 +644,7 @@ function onSuccess(position){
         alert(resLoc);
         $("#map-canvas").html(resLoc+" ^^^^^^^^^^^^");
       }
-    });*/
+    });
     //var LatLong = new google.maps.LatLng(latitude,longitude);
     //alert(LatLong);
 
--- www/js/app.js
+++ www/js/app.js
@@ -610,10 +610,11 @@ function onSuccess(position){
     var longitude = position.coords.longitude;
     var latitude = position.coords.latitude;
     //alert("https://maps.googleapis.com/maps/api/place/textsearch/json?query="+latitude+longitude+"&key=AIzaSyCfIHJQnEnmC-s6OO9qaymRe6dKG4l0T1s");
-     $.ajax({
+     /*$.ajax({
       type:'GET',
       dataType: "json", 
-      url:"https://maps.googleapis.com/maps/api/place/textsearch/json?query="+latitude+longitude+"&key=AIzaSyCfIHJQnEnmC-s6OO9qaymRe6dKG4l0T1s",
+      url:"https://maps.googleapis.com/maps/api/place/textsearch/json?query="+latitude+longitude+"&key=AIzaSyAdbMPmV9BqtRFcHEHCoe1fgpU2mEA1UnM",
+      //AIzaSyAdbMPmV9BqtRFcHEHCoe1fgpU2mEA1UnM (SIR) //
       //data:{'latitude':latitude,'longitude':longitude},
       success:function(resLoc){
         alert(resLoc);
@@ -630,12 +631,12 @@ function onSuccess(position){
         
         alert("add_array "+add_array);
         alert("add_array1 "+add_array1);*/
-        $("#map-canvas").html(resLoc+" ^^^^^^^^^^^^");
+ /*       $("#map-canvas").html(resLoc+" ^^^^^^^^^^^^");
         
       }
-    });
+    });*/
 
-    /*$.ajax({
+    $.ajax({
       type:'POST', 
       url:base_url+'APP/Appcontroller/getLocation',
       data:{'latitude':latitude,'longitude':longitude},
@@ -643,7 +644,7 @@ function onSuccess(position){
         alert(resLoc);
         $("#map-canvas").html(resLoc+" ^^^^^^^^^^^^");
       }
-    });*/
+    });
     //var LatLong = new google.maps.LatLng(latitude,longitude);
     //alert(LatLong);
 
