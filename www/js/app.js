793f5a291e350c7d94089fbe9b45af689ef01861
HinaYashvi <hina.parmar@staroneweb.co.in>
2020-01-20 10:37:46 +0530

    first commit

--- www/js/app.js
+++ www/js/app.js
@@ -791,24 +791,27 @@ var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: -33.8688, lng: 151.2195},
       zoom: 13
     });
+alert(map);
 var input = document.getElementById('search');
 map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
- 
+ alert(input);
 var autocomplete = new google.maps.places.Autocomplete(input);
 autocomplete.bindTo('bounds', map);
  
-varinfowindow = new google.maps.InfoWindow();
+var infowindow = new google.maps.InfoWindow();
+alert("infowindow "+infowindow);
 var marker = new google.maps.Marker({
         map: map,
 anchorPoint: new google.maps.Point(0, -29)
     });
  
 autocomplete.addListener('place_changed', function() {
+  alert("in place_changed");
 infowindow.close();
 marker.setVisible(false);
 var place = autocomplete.getPlace();
         if (!place.geometry) {
-window.alert("Autocomplete's returned place contains no geometry");
+alert("Autocomplete's returned place contains no geometry");
             return;
         }
  
